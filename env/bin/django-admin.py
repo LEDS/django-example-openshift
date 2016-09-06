765c58bf49f85ae042f829b48bbe74e3be4d1338
paulosantosjunior <paulossjunior@gmail.com>
2016-09-06 00:48:16 -0300

    atualizando o projeto

--- env/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/paulo/Documents/projects/python/django-example-openshift/env/bin/python3.4
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
